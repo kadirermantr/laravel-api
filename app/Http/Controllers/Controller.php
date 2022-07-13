7849b30842b3376e84bb66056832e12e27ac1ea2
kadir-erman <kadirerman@teknasyon.com>
2022-07-14 00:36:49 +0300

    Add return functions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function returnSuccess($data): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'data' => $data,
+        ]);
+    }
+
+    public function returnFalse(?int $statusCode = 404): JsonResponse
+    {
+        return response()->json([
+            'success' => false,
+            'data' => null
+        ], $statusCode);
+    }
 }
