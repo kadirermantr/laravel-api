8e2dadc1d71670ff5287bcc87efd961bb2011e8e
kadir-erman <kadirerman@teknasyon.com>
2022-07-13 16:15:45 -0400

    Add new routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('user', UserController::class);
+Route::resource('book', BookController::class);
+Route::resource('author', AuthorController::class);
