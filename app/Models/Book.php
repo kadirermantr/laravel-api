fa3fb5fb2f4b183490429d99d457df15d9bdd062
kadir-erman <kadirerman@teknasyon.com>
2022-07-14 20:50:35 +0300

    Add fillable variable

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'page',
+        'language',
+    ];
 }
