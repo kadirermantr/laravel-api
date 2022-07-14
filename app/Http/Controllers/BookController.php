b2c916d56a7a210573829be82c1c566e97ee65f0
kadir-erman <kadirerman@teknasyon.com>
2022-07-14 20:48:02 +0300

    Remove unused class

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
-
 class BookController extends Controller
 {
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
-
 class UserController extends Controller
 {
 
