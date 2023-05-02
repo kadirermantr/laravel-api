ce1b3dc7977ccac9e77f68a2abef5eb11b2ea5ef
kadirerman <kadirermantr@gmail.com>
2023-05-02 16:00:52 +0300

    refactor: update user factory and use it

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,23 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => 'Admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('password'),
-        ]);
+        User::factory()->count(10)->create();
     }
 }
