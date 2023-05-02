878d394d1888107c3a314046861936fe26c3b76d
kadirerman <kadirermantr@gmail.com>
2023-05-02 16:39:10 +0300

    Revert "fix: change wrong status code function"
    This reverts commit 70b1d3abc7ac3cae55de88ab41748b5020148cb3.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -56,7 +56,7 @@ class Handler extends ExceptionHandler
         return Replier::responseFalse(
             null,
             $e->getMessage(),
-            $e->getCode()
+            $e->getStatusCode()
         );
     }
 }
