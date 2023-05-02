ca7b93d68dbd51806ae020b3a1a42b5f247c087a
kadirerman <kadirermantr@gmail.com>
2023-05-02 15:29:42 +0300

    build: upgrade laravel version to 10.8 from 9.22

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,10 +21,8 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        $this->registerPolicies();
-
         //
     }
 }
