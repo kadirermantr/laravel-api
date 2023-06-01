eebd2db99e8a3ea8a0efbbdbced02267b9e3c6da
kadirerman <kadirermantr@gmail.com>
2023-06-01 05:06:09 +0300

    refactor: group controllers

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\AuthorController;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ {
+    AuthorController,
+    BookController,
+    CategoryController,
+    UserController
+};
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
